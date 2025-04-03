6f941a69146b5cb29ca0fab829b887cb2845223d
EndlessRecess <36823895+EndlessRecess@users.noreply.github.com>
2025-04-02 20:58:52 -0700

    updated to selectFormatCoercion etc from selectType

--- src/routes/transfer/+page.svelte
+++ src/routes/transfer/+page.svelte
@@ -18,18 +18,19 @@
 	<pre>
 		{JSON.stringify(
 			importedData.columns.map((col) => ({
-				headerName: col.headerName,
-				type: col.type,
-				isToggled: col.isToggled,
-				isMapped: col.isMapped,
-				mappedTo: col.mappedTo,
-				isFormatted: col.isFormatted,
-				selectTypeCoercion: col.selectTypeCoercion,
-				wasTypeCoerced: col.wasTypeCoerced,
-				values: col.values.slice(0, 3) // Show first 3 values for brevity
+			  headerName: col.headerName,
+			  type: col.type,
+			  currentFormat: col.currentFormat, // Add this
+			  isToggled: col.isToggled,
+			  isMapped: col.isMapped,
+			  mappedTo: col.mappedTo,
+			  isFormatted: col.isFormatted,
+			  selectFormatCoercion: col.selectFormatCoercion, // Updated name
+			  wasFormatCoerced: col.wasFormatCoerced, // Updated name
+			  values: col.values.slice(0, 3)
 			})),
 			null,
 			2
-		)}
+		  )}
 	  </pre>
 {/if}
